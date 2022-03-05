6eeae575fc0810220dbe901e50d202168c7c1e98
David <davidda@mta.ac.il>
2022-03-05 21:18:56 +0200

    fff

--- ex1_q1.c
+++ ex1_q1.c
@@ -3,19 +3,20 @@
 #include "string.h"
 #include "stdbool.h"
 
-
-typedef struct student {
-  char name[11];
-  int grades[10];
-  int numOfGrades;
+typedef struct student
+{
+    char name[11];
+    int grades[10];
+    int numOfGrades;
 } student;
 
 student *students;
 int numOfStudents = 0;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-    // init students array 
+    // init students array
     students = (student *)malloc(sizeof(student) * 16); // todo: add dynamic allocation
 
     // read and handle input
@@ -28,22 +29,28 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void sortGrades(){
-    for (int i = 0; i < numOfStudents; i++) {
+void sortGrades()
+{
+    for (int i = 0; i < numOfStudents; i++)
+    {
         qsort(students[i].grades, students[i].numOfGrades, sizeof(int), compare);
     }
 }
 
-int compare(const void *a, const void *b){
+int compare(const void *a, const void *b)
+{
     return *(int *)a - *(int *)b;
 }
 
-void writeToFile() {
+void writeToFile()
+{
     FILE *fp;
     fp = fopen("merged.txt", "w");
-    for (int i = 0; i < numOfStudents; i++) {
+    for (int i = 0; i < numOfStudents; i++)
+    {
         fprintf(fp, "%s ", students[i].name);
-        for (int j = 0; j < students[i].numOfGrades; j++) {
+        for (int j = 0; j < students[i].numOfGrades; j++)
+        {
             fprintf(fp, "%d ", students[i].grades[j]);
         }
         fprintf(fp, "\n");
@@ -51,26 +58,34 @@ void writeToFile() {
     fclose(fp);
 }
 
-void readAllDataFromFiles(int numOfFiles , char* argv[]) {
-    //read all file names form argv
+void readAllDataFromFiles(int numOfFiles, char *argv[])
+{
+    // read all file names form argv
     char *fileName = malloc(sizeof(char) * numOfFiles);
-    for (int i = 1; i < numOfFiles; i++) {
-        strcpy(fileName[i-1], argv[i]);
+    for (int i = 1; i < numOfFiles; i++)
+    {
+        strcpy(fileName[i - 1], argv[i]);
         printf("%s\n", fileName); // for denugging
     }
 
-    //read all lines form files
-    for (int i = 0; i < numOfFiles; i++) {
-        FILE* fp = fopen(fileName[i], "r");
-        if (fp == NULL) {
+    // read all lines form files
+    for (int i = 0; i < numOfFiles; i++)
+    {
+        FILE *fp = fopen(fileName[i], "r");
+        if (fp == NULL)
+        {
             printf("Error Reading File\n");
             exit(1);
         }
         char *line = malloc(sizeof(char) * 81);
-        while (fgets(line, 81, fp) != NULL) {
-            if(line[0] == '\n'){
+        while (fgets(line, 81, fp) != NULL)
+        {
+            if (line[0] == '\n')
+            {
                 continue;
-            } else {
+            }
+            else
+            {
                 addLineToStudent(line);
             }
         }
@@ -78,17 +93,22 @@ void readAllDataFromFiles(int numOfFiles , char* argv[]) {
     }
 }
 
-void addLineToStudent(char* studentData) {
-    char* name = strtok(studentData, " ");
+void addLineToStudent(char *studentData)
+{
+    char *name = strtok(studentData, " ");
 
-    if (!findIfStudentExists(name)) {
+    if (!findIfStudentExists(name))
+    {
         createAndAddStudent(name);
     }
 
-    for (int i = 0; i < numOfStudents; i++) {
-        if (strcmp(students[i].name, name) == 0) {
+    for (int i = 0; i < numOfStudents; i++)
+    {
+        if (strcmp(students[i].name, name) == 0)
+        {
             char *grades = strtok(NULL, " ");
-            while (grades != NULL) {
+            while (grades != NULL)
+            {
                 students[i].grades[students[i].numOfGrades] = atoi(grades);
                 grades = strtok(NULL, " ");
                 students[i].numOfGrades++;
@@ -99,32 +119,35 @@ void addLineToStudent(char* studentData) {
     }
 }
 
-void createAndAddStudent(char * name){
+void createAndAddStudent(char *name)
+{
     student *student = malloc(sizeof(student));
     strcpy(student->name, name);
     student->numOfGrades = 0;
     numOfStudents++;
 }
 
-bool findIfStudentExists(name){
+bool findIfStudentExists(name)
+{
     for (int i = 0; i < numOfStudents; i++)
     {
-        if(strcmp(students[i].name, name) == 0){
+        if (strcmp(students[i].name, name) == 0)
+        {
             return true;
         }
     }
     return false;
 }
 
-
-
 // ---------------------------------------------- tzvi report format ----------------------------------------------------- //
-void report_input_file(const char *file_name, int num_stud) {
+void report_input_file(const char *file_name, int num_stud)
+{
     fprintf(stderr, "process: %d file: %s number of students: %d\n",
             getpid(), file_name, num_stud);
 }
 
-void report_data_summary(int num_stud, double avg) {
+void report_data_summary(int num_stud, double avg)
+{
     fprintf(stderr, "process: %d data summary - number of students:"
                     " %d grade average: %.2f\n",
             getpid(), num_stud, avg);
