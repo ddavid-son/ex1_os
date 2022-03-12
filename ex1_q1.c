6fdfe1f5aca5a59ccdbe12d3adfc556cc3fce45a
David Davidson <ddavid8888@outlook.com>
2022-03-13 00:00:02 +0200

    fixed allocating lines inside loops fixed more names in q1

--- ex1_q1.c
+++ ex1_q1.c
@@ -15,48 +15,41 @@ student *students;
 int studs_num = 0;
 int arr_size = 16;
 
-void sort_grades();
-int compare(const void *a, const void *b);
-void write_to_file();
-void get_stud_data(int numOfFiles, char *argv[]);
-void add_grades(char *studentData);
-void resize();
-void create_new_stud(char *name);
-bool stud_exists(char* name);
-void report_input_file(const char *file_name, int num_stud);
-void report_data_summary(int num_stud, double avg);
-void printAllStudents();
 double calc_avg();
+void   sort_grades();
+void   write_to_file();
+bool   stud_exists(char* name);
+void   create_new_stud(char *name);
+void   add_grades(char *studentData);
+int    compare(const void *a, const void *b);
+void   get_stud_data(int numOfFiles, char *argv[]);
+void   report_data_summary(int num_stud, double avg);
+void   report_input_file(const char *file_name, int num_stud);
 
 int main(int argc, char *argv[])
 {
-    // init students array
     students = (student *)malloc(sizeof(student) * 100);
-
-    // read and handle input
     get_stud_data(argc, argv);
 
     sort_grades();
     report_data_summary(studs_num,calc_avg());
 
-    // write student array to file merged.txt
     write_to_file();
-
     return 0;
 }
 
 double calc_avg(){
     int sum = 0;
-    int numOfGrades = 0;
+    int grades_num = 0;
     for (int i = 0; i < studs_num; i++)
     {
         for (int j = 0; j < students[i].grades_num ; j++)
         {
             sum+= students[i].grades[j];
-            numOfGrades++;
+            grades_num++;
         }
     }
-    return (double)sum / numOfGrades;    
+    return (double)sum / grades_num;    
 }
 
 void sort_grades()
@@ -88,10 +81,11 @@ void write_to_file()
     fclose(fp);
 }
 
-void get_stud_data(int numOfFiles, char *argv[])
+void get_stud_data(int file_num, char *argv[])
 {
-    int numOfStudnetInfile=0;
-    for (int i = 1; i < numOfFiles; i++)
+    int stud_in_file = 0;
+    char *line = malloc(sizeof(char) * 81);
+    for (int i = 1; i < file_num; i++)
     {
         FILE *fp = fopen(argv[i], "r");
         if (fp == NULL) 
@@ -99,7 +93,6 @@ void get_stud_data(int numOfFiles, char *argv[])
             printf("Error Reading File\n");
             exit(1);
         }
-        char *line = malloc(sizeof(char) * 81);
         while (fgets(line, 81, fp) != NULL)
         {
             if (line[0] == '\n')
@@ -108,19 +101,21 @@ void get_stud_data(int numOfFiles, char *argv[])
             }
             else
             {
-                numOfStudnetInfile++;
+                stud_in_file++;
                 add_grades(line);
             }
         }
         fclose(fp);
-        report_input_file(argv[i], numOfStudnetInfile);
-        numOfStudnetInfile = 0;
+        report_input_file(argv[i], stud_in_file);
+        stud_in_file = 0;
     }
+    free(line);
 }
 
-void add_grades(char *studentData)
+void add_grades(char *stud_data)
 {
-    char *name = strtok(studentData, " ");
+    char *name = strtok(stud_data, " ");
+    char *grades;
     if (!stud_exists(name))
     {
         create_new_stud(name);
@@ -130,7 +125,7 @@ void add_grades(char *studentData)
     {
         if (strcmp(students[i].name, name) == 0)
         {
-            char *grades = strtok(NULL, " ");
+            grades = strtok(NULL, " ");
             while (grades != NULL && (*grades) != '\n' && (*grades) != '\r')
             {
                 students[i].grades[students[i].grades_num] = atoi(grades);
@@ -140,14 +135,6 @@ void add_grades(char *studentData)
 
             return;
         }
-    }
-}
-
-void resize(){
-    if (studs_num == arr_size-1)
-    {
-        arr_size *= 2;
-        students = (student *)realloc(students, sizeof(student) * arr_size);
     }
 }
 
